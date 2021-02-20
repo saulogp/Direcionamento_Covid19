e79ba7963265ab9adc20644283b4fca78be2b72b
ThiagoMacola <thiago_macola@hotmail.com>
2021-02-19 21:46:16 -0300

    Implementando Classe Triagem e Classe Pessoa

---
+++ Direcionamento_Covid19/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direcionamento_Covid19
+{
+	class Pessoa
+	{
+		public string Nome { get; set; }
+		public int Idade { get; set; }
+		public string Sexo { get; set; }
+		public long Cpf { get; set; }
+		public long CarteiraVacin { get; set; }
+		public bool Comorbidade { get; set; }
+		public int TempoDeSintomas { get; set; }
+		public bool Positivo { get; set; }
+		public Pessoa Proximo { get; set; }
+
+		public override string ToString()
+		{
+			return ">>>>>>> Dados do Paciente <<<<<<\nNome: " + Nome +
+				"\nIdade: " + Idade + "\nSexo: " + Sexo +
+				"\nCPF: " + Cpf + "\nCarteira de Vacinação: " + CarteiraVacin +
+				"\nComorbidade: " + Comorbidade + "\nTempo de Sintomas: " + TempoDeSintomas +
+				"\nPositivo: " + Positivo;
+		}
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ Direcionamento_Covid19/Triagem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direcionamento_Covid19
+{
+	class Triagem
+	{
+		public Pessoa DadosPessoas { get; set; }
+
+		public void Cadastramento()
+		{
+			Console.Write("Paciente tem alguma comorbidade?: ");
+			bool comorbidade = bool.Parse(Console.ReadLine());
+			Console.Write("Quanto tempo está com o sintomas?: ");
+			int tempo = int.Parse(Console.ReadLine()); 
+			Pessoa p = new Pessoa
+			{
+				Comorbidade = comorbidade,
+				TempoDeSintomas = tempo
+			};
+		}
+		
+		public int Exame()
+		{
+			int resultado; 
+			Console.WriteLine("Coletando amostras para exame!!");
+			Console.WriteLine("Processando exame... aguarde!");
+			Console.WriteLine("Menu para resultados de exame: ");
+			do
+			{
+				Console.WriteLine("1 - Positivo: ");
+				Console.WriteLine("2 - Assintomatico ");
+				Console.WriteLine("3 - Negativo");
+				resultado = int.Parse(Console.ReadLine());
+				Console.Write("Digite de 1 a 3 para inserir possiveis resultados do exame: ");
+			} while ((resultado > 0) && (resultado < 4));
+
+			if (resultado == 1)
+				return 1;
+			else if (resultado == 2)
+				return 2;
+			else
+				return 3;
+
+		}
+	}
+}
